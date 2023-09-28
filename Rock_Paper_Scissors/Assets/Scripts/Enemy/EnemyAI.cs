fcb48ad3c80448254a532d2300f0f41befaa6ee8
Yitzchak Cohen <yitzchakncohen@gmail.com>
2023-09-27 20:02:26 -0400

    Separate State Scripts

--- Rock_Paper_Scissors/Assets/Scripts/Enemy/EnemyAI.cs
+++ Rock_Paper_Scissors/Assets/Scripts/Enemy/EnemyAI.cs
@@ -5,26 +5,9 @@ using System.Threading.Tasks;
 
 public class EnemyAI : MonoBehaviour
 {
-    private enum State 
-    {
-        WaitingForTurn,
-        FindingAction,
-        TakingTurn, 
-        BusyTakingAction,
-    }
-
-    private State state;
+    private EnemyStatePattern state = new EnemyStatePattern();
     private TurnManager turnManager;
     private UnitManager unitManager;
-    private EnemyAIAction nextAction = null;
-    private bool nextActionFound = false;
-    private bool findingNextAction = false;
-    private float timer;
-
-    private void Awake() 
-    {
-        state = State.WaitingForTurn;
-    }
 
     private void Start() 
     {
@@ -37,51 +20,16 @@ public class EnemyAI : MonoBehaviour
     {
         if(turnManager.IsPlayerTurn()){ return; }
 
-        switch (state)
+        switch (state.GetCurrentState())
         {
-            case State.WaitingForTurn:
+            case WaitingForTurnState:
                 break;
-            case State.FindingAction:
-                FindAction();
+            case FindingActionState:
+                state.FindAction(unitManager);
                 break;
-            case State.TakingTurn:
-                TakeAction();
+            case TakingActionState:
+                state.TakeAction(CompleteAction, turnManager);
                 break;
-            case State.BusyTakingAction:
-                break;
-        }
-    }
-
-    private void FindAction()
-    {
-        // Debug.Log("AI searching for action...");
-        if (nextActionFound)
-        {
-            state = State.TakingTurn;
-        }
-        else
-        {
-            if (!findingNextAction)
-            {
-                FindNextAction();
-            }
-        }
-    }
-    
-    private void TakeAction()
-    {
-        timer -= Time.deltaTime;
-        if (timer <= 0f)
-        {
-            if (TryTakeEnemyAIAction(CompleteAction))
-            {
-                state = State.BusyTakingAction;
-            }
-            else
-            {
-                // No more enemies have actions they can take so end the turn. 
-                turnManager.NextTurn();
-            }
         }
     }
 
@@ -89,96 +37,12 @@ public class EnemyAI : MonoBehaviour
     {
         if(!turnManager.IsPlayerTurn())
         {
-            nextActionFound = false;
-            state = State.FindingAction;
+            state.StartTurn();
         }
     }
 
     private void CompleteAction()
     {
-        timer = 0.5f;
-        nextActionFound = false;
-        state = State.FindingAction;
-    }
-
-    private async void FindNextAction()
-    {
-        findingNextAction = true;
-        
-        nextAction = await GetBestEnemyAction();
-
-        findingNextAction = false;
-        nextActionFound = true;
-    }
-
-    private bool TryTakeEnemyAIAction(Action onEnemyAIActionComplete)
-    {
-        // float startTime = Time.realtimeSinceStartup;
-
-        if (nextAction != null)
-        {
-            // Debug.Log("Taking action with value: " + bestEnemeyAIAction.actionValue);
-            // Debug.Log("Action Found: " + (Time.realtimeSinceStartup - startTime) * 1000f);
-            return nextAction.unitAction.TryTakeAction(nextAction.gridObject, onEnemyAIActionComplete);
-        }
-
-        // Debug.Log("No Action Found: " + (Time.realtimeSinceStartup - startTime) * 1000f);
-        return false;
-    }
-
-    private async Task<EnemyAIAction>GetBestEnemyAction()
-    {
-        EnemyAIAction bestEnemeyAIAction = null;
-
-        // Get the best action from each unit and see if it is the best.
-        foreach (Unit enemyUnit in unitManager.GetEnemyUnitsList())
-        {
-            if (bestEnemeyAIAction == null)
-            {
-                bestEnemeyAIAction = await GetBestActionForUnit(enemyUnit);
-            }
-            else
-            {
-                EnemyAIAction testAction = await GetBestActionForUnit(enemyUnit);
-                if (testAction != null && testAction.actionValue > bestEnemeyAIAction.actionValue)
-                {
-                    bestEnemeyAIAction = testAction;
-                }
-            }
-        }
-
-        return bestEnemeyAIAction;
-    }
-
-    private async Task<EnemyAIAction> GetBestActionForUnit(Unit enemyUnit)
-    {
-        await Task.Yield();
-
-        EnemyAIAction bestEnemeyAIAction = null;
-
-        foreach (UnitAction baseAction in enemyUnit.GetUnitActions())
-        {
-            if(baseAction.GetActionPointsRemaining() <= 0)
-            {
-                // Enemy cannot afford this action
-                continue;
-            }
-
-            // Find the best of the best.
-            if(bestEnemeyAIAction == null)
-            {
-                bestEnemeyAIAction = baseAction.GetBestEnemyAIAction();
-            }
-            else
-            {
-                EnemyAIAction testEnemyAIAction = baseAction.GetBestEnemyAIAction();
-
-                if(testEnemyAIAction != null && testEnemyAIAction.actionValue > bestEnemeyAIAction.actionValue)
-                {
-                    bestEnemeyAIAction = testEnemyAIAction;
-                }
-            }
-        }
-        return bestEnemeyAIAction;
+        state.CompleteAction();
     }
 }
--- Rock_Paper_Scissors/Assets/Scripts/Enemy/EnemyStatePattern.cs
+++ Rock_Paper_Scissors/Assets/Scripts/Enemy/EnemyStatePattern.cs
@@ -12,18 +12,18 @@ public interface EnemyState
 {
     void StartTurn(EnemyStateContext context);
     void FindAction(EnemyStateContext context, UnitManager unitManager);
-    void TakeAction(EnemyStateContext context, Action CompleteAction);
+    void TakeAction(EnemyStateContext context, Action CompleteAction, TurnManager turnManager);
     void CompleteAction(EnemyStateContext context);
     void EndTurn(EnemyStateContext context);
 }
 
-public class EnemyStatePattern : MonoBehaviour, EnemyStateContext
+public class EnemyStatePattern : EnemyStateContext
 {
-    EnemyState currentState = new WaitingForTurnState();
+    private EnemyState currentState = new WaitingForTurnState();
 
     public void StartTurn() => currentState.StartTurn(this);
     public void FindAction(UnitManager unitManager) => currentState.FindAction(this, unitManager);
-    public void TakeAction(Action CompleteAction) => currentState.TakeAction(this, CompleteAction);
+    public void TakeAction(Action CompleteAction, TurnManager turnManager) => currentState.TakeAction(this, CompleteAction, turnManager);
     public void CompleteAction() => currentState.CompleteAction(this);
     public void EndTurn() => currentState.EndTurn(this);
 
@@ -31,193 +31,9 @@ public class EnemyStatePattern : MonoBehaviour, EnemyStateContext
     {
         currentState = newState;
     }
-}
 
-public class WaitingForTurnState : EnemyState
-{
-    public void StartTurn(EnemyStateContext context)
+    public EnemyState GetCurrentState()
     {
-        context.SetState(new FindingActionState());
-    }
-
-    public void FindAction(EnemyStateContext context, UnitManager unitManager)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void TakeAction(EnemyStateContext context, Action CompleteAction)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void CompleteAction(EnemyStateContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void EndTurn(EnemyStateContext context)
-    {
-        throw new NotImplementedException();
+        return currentState;
     }
 }
-
-public class FindingActionState : EnemyState
-{
-    private UnitManager unitManager;
-    
-    public void StartTurn(EnemyStateContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void FindAction(EnemyStateContext context, UnitManager unitManager)
-    {
-        this.unitManager = unitManager;
-
-        FindNextAction(context);
-    }
-
-    public void TakeAction(EnemyStateContext context, Action CompleteAction)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void CompleteAction(EnemyStateContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void EndTurn(EnemyStateContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    private async void FindNextAction(EnemyStateContext context)
-    {
-        EnemyAIAction nextAction = await GetBestEnemyAction();
-
-        context.SetState(new TakingActionState(nextAction));
-    }
-
-    private async Task<EnemyAIAction>GetBestEnemyAction()
-    {
-        EnemyAIAction bestEnemeyAIAction = null;
-
-        // Get the best action from each unit and see if it is the best.
-        foreach (Unit enemyUnit in unitManager.GetEnemyUnitsList())
-        {
-            if (bestEnemeyAIAction == null)
-            {
-                bestEnemeyAIAction = await GetBestActionForUnit(enemyUnit);
-            }
-            else
-            {
-                EnemyAIAction testAction = await GetBestActionForUnit(enemyUnit);
-                if (testAction != null && testAction.actionValue > bestEnemeyAIAction.actionValue)
-                {
-                    bestEnemeyAIAction = testAction;
-                }
-            }
-        }
-
-        return bestEnemeyAIAction;
-    }
-
-    private async Task<EnemyAIAction> GetBestActionForUnit(Unit enemyUnit)
-    {
-        await Task.Yield();
-
-        EnemyAIAction bestEnemeyAIAction = null;
-
-        foreach (UnitAction baseAction in enemyUnit.GetUnitActions())
-        {
-            if(baseAction.GetActionPointsRemaining() <= 0)
-            {
-                // Enemy cannot afford this action
-                continue;
-            }
-
-            // Find the best of the best.
-            if(bestEnemeyAIAction == null)
-            {
-                bestEnemeyAIAction = baseAction.GetBestEnemyAIAction();
-            }
-            else
-            {
-                EnemyAIAction testEnemyAIAction = baseAction.GetBestEnemyAIAction();
-
-                if(testEnemyAIAction != null && testEnemyAIAction.actionValue > bestEnemeyAIAction.actionValue)
-                {
-                    bestEnemeyAIAction = testEnemyAIAction;
-                }
-            }
-        }
-        return bestEnemeyAIAction;
-    }
-}
-
-public class TakingActionState : EnemyState
-{
-    private EnemyAIAction nextAction = null;
-    private TurnManager turnManager;
-    private float timer = 0.5f;
-
-    public TakingActionState(EnemyAIAction nextAction)
-    {
-        this.nextAction = nextAction;
-    }
-
-    public void StartTurn(EnemyStateContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void CompleteAction(EnemyStateContext context)
-    {
-        context.SetState(new FindingActionState());
-    }
-
-    public void FindAction(EnemyStateContext context, UnitManager unitManager)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void TakeAction(EnemyStateContext context, Action CompleteAction)
-    {
-        timer -= Time.deltaTime;
-        if (timer <= 0f)
-        {
-
-            if (TryTakeEnemyAIAction(CompleteAction))
-            {
-                // Action successfully taken
-            }
-            else
-            {
-                // No more enemies have actions they can take so end the turn. 
-                EndTurn(context);
-            }
-        }
-    }
-
-    public void EndTurn(EnemyStateContext context)
-    {
-        turnManager.NextTurn();
-    }
-
-    private bool TryTakeEnemyAIAction(Action onEnemyAIActionComplete)
-    {
-        // float startTime = Time.realtimeSinceStartup;
-
-        if (nextAction != null)
-        {
-            // Debug.Log("Taking action with value: " + bestEnemeyAIAction.actionValue);
-            // Debug.Log("Action Found: " + (Time.realtimeSinceStartup - startTime) * 1000f);
-            return nextAction.unitAction.TryTakeAction(nextAction.gridObject, onEnemyAIActionComplete);
-        }
-
-        // Debug.Log("No Action Found: " + (Time.realtimeSinceStartup - startTime) * 1000f);
-        return false;
-    }
-}
-
