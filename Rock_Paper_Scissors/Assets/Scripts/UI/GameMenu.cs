0b88463d706504eaaffd15ac2d86bd6ae013650b
Yitzchak Cohen <yitzchakncohen@gmail.com>
2024-04-03 18:01:27 -0400

    DOTween Added
    Animated Game Over Panel

--- Rock_Paper_Scissors/Assets/Scripts/UI/GameMenu.cs
+++ Rock_Paper_Scissors/Assets/Scripts/UI/GameMenu.cs
@@ -1,10 +1,11 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using RockPaperScissors;
+using RockPaperScissors.UI;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class GameMenu : MonoBehaviour
 {
@@ -17,10 +18,13 @@ public class GameMenu : MonoBehaviour
     [SerializeField] private TextMeshProUGUI gameOverHighScoreValueText;
     [SerializeField] private Button[] MainMenuButtons;
     [SerializeField] private Button NewGameButton;
-
+    [SerializeField] private LetterAnimation gameOverTextAnimation;
+    [SerializeField] private float gameOverAnimationTime = 0.5f;
+    private RectTransform gameOverMenuRectTransform;
 
     private void Start() 
     {
+        gameOverMenuRectTransform = gameOverMenuPanel.GetComponent<RectTransform>();
         GameplayManager.OnGameOver += GameplayManager_OnGameOver;
         foreach (Button button in MainMenuButtons)
         {
@@ -55,12 +59,25 @@ public class GameMenu : MonoBehaviour
         AudioManager.Instance.PlayMenuNavigationSound();
     }
 
-    private void OpenGameOverMenu(int score, int highscore)
+    [ContextMenu("Open Game Over Menu")]
+    public void TestMenu()
+    {
+        OpenGameOverMenu(100, 1000);
+    }
+
+    public void OpenGameOverMenu(int score, int highscore)
     {
         HUDPanel.SetActive(false);
         gameOverMenuPanel.SetActive(true);
         gameOverScoreValueText.text = score.ToString();
         gameOverHighScoreValueText.text = highscore.ToString();
+        gameOverMenuRectTransform.transform.localPosition = new Vector2(0, -Screen.height);
+        Sequence gameOverSequence = DOTween.Sequence();
+        gameOverSequence.Append(gameOverMenuRectTransform.DOAnchorPos(Vector2.zero, gameOverAnimationTime).SetEase(Ease.InOutQuint));
+        gameOverSequence.AppendCallback(() => {
+            gameOverTextAnimation.Play();
+        });
+        gameOverSequence.PlayForward();
     }
 
     private void CloseGameOverMenu()
